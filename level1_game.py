1858dcf6500f4a47234bdf285f8069d4e9f44644
Presley Black <presleyblack24@gmail.com>
2023-12-13 09:58:56 -0600

    finished level1 game with python

--- level1_game.py
+++ level1_game.py
@@ -23,13 +23,58 @@
 
 # Try to figure out what this code is doing and how it might be useful to you
 import random
-digits = list(range(10))
-random.shuffle(digits)
-print(digits[:3])
 
-# Another hint:
-guess = input("What is your guess? ")
-print(guess)
 
 # Think about how you will compare the input to the random number, what format
 # should they be in? Maybe some sort of sequence? Watch the Lecture video for more hints!
+
+
+# Get Guess
+def get_guess():
+    return list(input("What is your guess? "))
+
+
+# Generate computer code
+def generate_numbers():
+    digits = [str(num) for num in range(10)]
+    random.shuffle(digits)
+    return digits[:3]
+
+
+# Generate the clues
+
+def generate_clues(code, user_guess):
+    if user_guess == code:
+        return "Code Cracked!"
+
+    clues = []
+    # Unpacking the index location and number itself
+    for ind, num in enumerate(user_guess):
+        if num == code[ind]:  # If the number is equal to the code index location.
+            clues.append("Match")
+        elif num in code:  # If any number is in the code
+            clues.append("Close")
+
+    if clues == []:  # If there is no same numbers.
+        return ["Nope"]
+    else:
+        return clues
+
+
+# Run Game Logic
+
+print("Welcome to the game!")
+
+secret_code = generate_numbers()
+# print(secret_code)
+
+clue_report = []
+
+while clue_report != "Code Cracked!":
+
+    guess = get_guess()
+
+    clue_report = generate_clues(guess, secret_code)
+    print("Here is the result of your guess: ")
+    for clue in clue_report:
+        print(clue)
