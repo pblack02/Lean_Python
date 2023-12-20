7f0e1049c71fbf8dadb961bbe2611321a4cd1537
Presley Black <presleyblack24@gmail.com>
2023-12-20 16:00:38 -0600

    Finished MTV paradigm lesson

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me': "Hello i'm from views.py/ new"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    # my_dict = {'insert_me': "Hello i'm from views.py/ new"}
+    return render(request, 'first_app/index.html', context=date_dict)
 
 
