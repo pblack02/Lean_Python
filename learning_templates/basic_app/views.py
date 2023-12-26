781ac20e023aa0ee1a679e6b588c701f653be902
Presley Black <presleyblack24@gmail.com>
2023-12-26 15:29:18 -0600

    Exercise project: django template filters and costum filters lesson

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'basic_app/index.html')
+    context_dict = {'text': 'Hello World', 'number':100}
+    return render(request, 'basic_app/index.html', context_dict)
 
 
 def other(request):
